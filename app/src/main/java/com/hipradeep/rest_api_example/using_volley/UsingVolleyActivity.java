78dc06829e520778efd0e54a174bf826bb6bc5d1
Pradeep <pradeepmaurya.cse@gmail.com>
2021-12-04 17:29:27 +0530

    add volley

---
+++ app/src/main/java/com/hipradeep/rest_api_example/using_volley/UsingVolleyActivity.java
@@ -0,0 +1,16 @@
+package com.hipradeep.rest_api_example.using_volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hipradeep.rest_api_example.R;
+
+public class UsingVolleyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_using_volley);
+    }
+}
